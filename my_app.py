05b85181db6e89c2ec009a56c1a18d5c7556752d
nimA45 <74744507+nimA45@users.noreply.github.com>
2023-02-07 15:58:23 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
-from keras.models import load_model
 import numpy as np
+import joblib
+
+
 
 app = Flask(__name__)
 
-# Load the trained model
-model = load_model('fashion_mnist_model.h5')
+model = joblib.load('random_forest_model.joblib')
 labels = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,23 +16,20 @@ def index():
         image_data = request.form['image_data']
 
         # Split the data into a list of pixels
-        pixels = image_data.split(',')
+        pixels = np.array([int(pixel) for pixel in image_data.split(',')])
 
         # Check that the input data is the correct length
         if len(pixels) != 784:
             return 'Error: Input data must contain 784 pixels (28x28), {} given'.format(len(pixels))
 
-        # Convert the list of pixels to a numpy array
-        pixels = np.array(pixels, dtype=np.float32)
-
         # Reshape the array to match the model's expected input shape
-        image = pixels.reshape(1, 28, 28, 1)
+        pixels = pixels.reshape(1, 784)
 
         # Use the model to predict the class of the image
-        prediction = model.predict(image)
+        prediction = model.predict(pixels)
 
         # Get the class label with the highest probability
-        class_label_index = np.argmax(prediction)
+        class_label_index = prediction[0]
         class_label = labels[class_label_index]
 
         # Return the result to the user
@@ -39,6 +37,6 @@ def index():
 
     return render_template('index.html')
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
